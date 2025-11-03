1b5725956f05be89b0982b3cb65fdb02de162689
JAY PANDYA <160032931+1JAYPANDYA1@users.noreply.github.com>
2025-11-03 08:05:57 +0530

    changes in protected route

--- src/ProtectedRoute.jsx
+++ src/ProtectedRoute.jsx
@@ -1,9 +1,31 @@
-import { Navigate } from 'react-router-dom';
-import Cookies from 'js-cookie';
+// ProtectedRoute.jsx
+import { useEffect, useState } from "react";
+import { Navigate } from "react-router-dom";
+import axios from "axios";
 
 const ProtectedRoute = ({ element }) => {
-  const isAuthenticated = Cookies.get('token'); 
-  
+  const [isAuthenticated, setIsAuthenticated] = useState(null);
+  const API_URL = process.env.REACT_APP_API_URL;
+
+  useEffect(() => {
+    const checkAuth = async () => {
+      try {
+        const res = await axios.get(`${API_URL}/auth/check`, {
+          withCredentials: true,
+        });
+        setIsAuthenticated(true);
+      } catch (err) {
+        setIsAuthenticated(false);
+      }
+    };
+    checkAuth();
+  }, []);
+
+  if (isAuthenticated === null) {
+    // optional: show loading spinner while checking auth
+    return <div>Loading...</div>;
+  }
+
   return isAuthenticated ? element : <Navigate to="/login" replace />;
 };
 
